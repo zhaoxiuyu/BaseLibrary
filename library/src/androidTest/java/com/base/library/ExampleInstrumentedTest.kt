132fb17002f40d8fef482e7d9515221410faa735
USER-20170209IY\Administrator <zxy_it@yeah.net>
2019-09-11 15:55:13 +0800

    首次上传

---
+++ library/src/androidTest/java/com/base/library/ExampleInstrumentedTest.kt
@@ -0,0 +1,21 @@
+package com.base.library
+
+import android.content.Context
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.base.library.test", appContext.packageName)
+    }
+}
