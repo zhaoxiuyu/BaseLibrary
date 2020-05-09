f0d494d87c789b6c317f78502cf6c082dba96193
USER-20170209IY\Administrator <zxy_it@yeah.net>
2020-05-09 15:52:45 +0800

    尝试一下ViewPage2

--- app/src/main/java/com/base/app/module/newfunction/ui/Fragment2.kt
+++ app/src/main/java/com/base/app/module/newfunction/ui/Fragment2.kt
@@ -1,10 +1,12 @@
 package com.base.app.module.newfunction.ui
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.base.app.R
 import com.base.app.module.newfunction.viewmodel.FunctionViewModel
 import com.base.library.mvvm.core.VMFragment
 import com.blankj.utilcode.util.LogUtils
+import com.blankj.utilcode.util.ToastUtils
 import kotlinx.android.synthetic.main.item_vp2.*
 
 class Fragment2 : VMFragment<FunctionViewModel>() {
@@ -24,6 +26,9 @@ class Fragment2 : VMFragment<FunctionViewModel>() {
         itemTv.setOnClickListener {
             getSharedViewModel()?.getEventLiveData()?.value = Triple("Fragment2", true, "测试")
         }
+        getSharedViewModel()?.getEventLiveData()?.observe(this, Observer {
+            ToastUtils.showLong("我看看收到没")
+        })
     }
 
     override fun onStart() {
